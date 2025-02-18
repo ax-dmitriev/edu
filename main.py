a1aa2963992731b40211bef79cd07aa38ca3ecec
Alexander <rench3@ya.ru>
2025-02-18 20:05:43 +0500

    4Д - Подзапросы

--- main.py
+++ main.py
@@ -6,26 +6,16 @@ con = sqlite3.connect("db.db")
 cursor = con.cursor()
 
 
-#! COUNT
-cursor.execute("SELECT COUNT(*) FROM Users")
-total_users = cursor.fetchone()[0]
-print(total_users)
+#! Наибольший возраст
+cursor.execute("""
+SELECT username, age
+FROM Users
+WHERE age = (SELECT MAX(age) FROM Users)
+""")
+oldtest_users = cursor.fetchall()
 
-#! SUM
-cursor.execute("SELECT SUM(age) FROM Users")
-total_sum = cursor.fetchone()[0]
-print(total_sum)
-
-#! MIN
-cursor.execute("SELECT MIN(age) FROM Users")
-total_min = cursor.fetchone()[0]
-print(total_min)
-
-#! MAX
-cursor.execute("SELECT MAX(age) FROM Users")
-total_max = cursor.fetchone()[0]
-print(total_max)
-
+for i in oldtest_users:
+    print(i)
 
 #! Сохранение изменений и закрытие соединения
 ##con.commit()
