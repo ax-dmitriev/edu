ee5caa4a0d04444f8e8b20f4f7f558ca4486fbbd
Alexander <rench3@ya.ru>
2025-02-18 19:31:37 +0500

    4 - Выполнение запроса

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ import sqlite3
 con = sqlite3.connect("db.db")
 cursor = con.cursor()
 
-#! Удаление пользователя
-cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
+##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser1", "us1@ya.ru", 28))
+##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser2", "us2@ya.ru", 29))
+##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser3", "us3@ya.ru", 30))
+##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser4", "us4@ya.ru", 31))
+##cursor.execute("INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser5", "us5@ya.ru", 32))
+
+#! Запрос
+cursor.execute("SELECT * FROM Users")
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
 
 #! Сохранение изменений и закрытие соединения
 con.commit()
