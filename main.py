23cd0ab9d6b1e71b823dcb08a2bbd5bef431cc62
Alexander <rench3@ya.ru>
2025-02-18 19:20:59 +0500

    2А - Создание индекса для Users (email)

--- main.py
+++ main.py
@@ -5,15 +5,8 @@ import sqlite3
 con = sqlite3.connect("db.db")
 cursor = con.cursor()
 
-#! Создание таблицы - Users
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER
-)
-""")
+#! Создание индекса для столбца email
+cursor.execute("CREATE INDEX idx_email ON Users (email)")
 
 #! Сохранение изменений и закрытие соединения
 con.commit()
