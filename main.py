caf002c599399f59faadb645da4e389932d9e0b0
Alexander <rench3@ya.ru>
2025-02-18 19:18:42 +0500

    2 - Создание базы данных

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+#! Подключение модулей
 import sqlite3
 
+#! Подключение к базе данных
 con = sqlite3.connect("db.db")
+cursor = con.cursor()
+
+#! Создание таблицы - Users
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+""")
+
+#! Сохранение изменений и закрытие соединения
+con.commit()
 con.close()
