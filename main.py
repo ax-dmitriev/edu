8c7855c7db6467d0ec5516ac32fe194209fecfe9
Alexander <rench3@ya.ru>
2025-02-18 20:03:02 +0500

    4C - Агрегатные функции

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ con = sqlite3.connect("db.db")
 cursor = con.cursor()
 
 
-#! Выбор и сортировка по возрасту по убыванию
-cursor.execute("""
-SELECT username, age, AVG(age)
-FROM Users
-GROUP BY age
-HAVING AVG(age) > ?
-ORDER BY age DESC
-""", (30,))
-res1 = cursor.fetchall()
-for row in res1:
-    print(row)
+#! COUNT
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+print(total_users)
+
+#! SUM
+cursor.execute("SELECT SUM(age) FROM Users")
+total_sum = cursor.fetchone()[0]
+print(total_sum)
+
+#! MIN
+cursor.execute("SELECT MIN(age) FROM Users")
+total_min = cursor.fetchone()[0]
+print(total_min)
+
+#! MAX
+cursor.execute("SELECT MAX(age) FROM Users")
+total_max = cursor.fetchone()[0]
+print(total_max)
 
 
 #! Сохранение изменений и закрытие соединения
