785cd290126158032cd3e5c19ac80fdc076f0517
Alexander <rench3@ya.ru>
2025-02-18 19:57:14 +0500

    4Б - Комбинированые запросы

--- main.py
+++ main.py
@@ -6,25 +6,14 @@ con = sqlite3.connect("db.db")
 cursor = con.cursor()
 
 
-###! Выбираем имена и возраст юзеров старше 25 лет
-##cursor.execute("SELECT username, age FROM Users WHERE age > ?", (25,))
-##res = cursor.fetchall()
-##for row in res:
-##    print(row)
-        
-###! Средний возраст для каждого возраста
-##cursor.execute("SELECT age, AVG(age) FROM Users GROUP BY age")
-##res1 = cursor.fetchall()
-##for row in res1:
-##    print(row)
-###! Фильтр по среднему более 30 лет
-##cursor.execute("SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?", (30,))
-##res2 = cursor.fetchall()
-##for row in res2:
-##    print(row)
-
 #! Выбор и сортировка по возрасту по убыванию
-cursor.execute("SELECT username, age FROM Users ORDER BY age DESC")
+cursor.execute("""
+SELECT username, age, AVG(age)
+FROM Users
+GROUP BY age
+HAVING AVG(age) > ?
+ORDER BY age DESC
+""", (30,))
 res1 = cursor.fetchall()
 for row in res1:
     print(row)
