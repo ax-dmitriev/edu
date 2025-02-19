4cacb8f02c3426736c53e37de03e506e28943c69
Alexander <rench3@ya.ru>
2025-02-19 13:07:45 +0500

    6 - Транзакции и управление данными

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-#! Подключение модулей
+##! Подключение модулей
+##import sqlite3
+##
+###! Подключение к базе данных
+##con = sqlite3.connect("db.db")
+##cursor = con.cursor()
+##
+##try:
+##    # Начало транзакции
+##    cursor.execute("BEGIN")
+##    # Операция
+##    cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user6", "user6@gmail.com"))
+##    cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user7", "user7@gmail.com"))
+##    # Подтверждение
+##    cursor.execute("COMMIT")
+##except:
+##    # Отмена
+##    cursor.execute("ROLLBACK")
+##
+##
+##
+##! Сохранение изменений и закрытие соединения
+##con.commit()
+##con.close()
+
+
 import sqlite3
 
-#! Подключение к базе данных
-con = sqlite3.connect("db.db")
-cursor = con.cursor()
-
-
-#! Выбираем всех юзера
-cursor.execute("""SELECT * FROM Users WHERE age IS NULL""")
-users = cursor.fetchall()
-
-for user in users:
-    print(user)
-
-
-#! Сохранение изменений и закрытие соединения
-##con.commit()
-con.close()
+with sqlite3.connect("db.db") as con:
+    cursor = con.cursor()
+    try:
+        with con:
+            cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user8", "user8@gmail.com"))
+            cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user9", "user9@gmail.com"))
+    except:
+        pass
