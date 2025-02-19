3b5366f2890f879cd077bdd97feabab5faa51aa4
Alexander <rench3@ya.ru>
2025-02-19 12:49:31 +0500

    5А - Методы получения данных

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ con = sqlite3.connect("db.db")
 cursor = con.cursor()
 
 
-#! Наибольший возраст
-cursor.execute("SELECT * FROM Users")
-users = cursor.fetchall()
-for i in users:
-    print(i)
+#! Выбираем первого юзера
+cursor.execute("""SELECT * FROM Users""")
+first_user = cursor.fetchone()
+print(first_user)
+
+#! Выбираем первых двух юзера
+cursor.execute("""SELECT * FROM Users""")
+first_3_user = cursor.fetchmany(3)
+print(first_3_user)
+
+#! Выбираем всех юзера
+cursor.execute("""SELECT * FROM Users""")
+all_users = cursor.fetchall()
+print(all_users)
+
 
 
 #! Сохранение изменений и закрытие соединения
