d1c4f1492446cee734c066883eda8b9161563e1a
Alexander <rench3@ya.ru>
2025-02-19 14:40:44 +0500

    7 - Новая таблица, триггеры и тд

--- main.py
+++ main.py
@@ -1,36 +1,70 @@
-##! Подключение модулей
+###! Модуль
 ##import sqlite3
 ##
-###! Подключение к базе данных
+###! Подключение, курсор
 ##con = sqlite3.connect("db.db")
 ##cursor = con.cursor()
 ##
-##try:
-##    # Начало транзакции
-##    cursor.execute("BEGIN")
-##    # Операция
-##    cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user6", "user6@gmail.com"))
-##    cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user7", "user7@gmail.com"))
-##    # Подтверждение
-##    cursor.execute("COMMIT")
-##except:
-##    # Отмена
-##    cursor.execute("ROLLBACK")
+###! Запрос
+##query = f"SELECT * FROM Users WHERE age > ?"
+##cursor.execute(query, (25,))
+##users = cursor.fetchall()
+##print(users)
 ##
-##
-##
-##! Сохранение изменений и закрытие соединения
-##con.commit()
 ##con.close()
 
 
+#! Модуль
+##import sqlite3
+##
+#! Подключение, курсор
+##con = sqlite3.connect("db.db")
+##cursor = con.cursor()
+##
+#! Запрос
+####query = f"SELECT * FROM Users WHERE age > ?"
+####cursor.execute(query, (25,))
+####users = cursor.fetchall()
+####print(users)
+##
+### Представление
+##cursor.execute("CREATE VIEW ActiveUsers AS SELECT * FROM Users WHERE is_active = 1")
+##cursor.execute("SELECT * FROM ActiveUsers")
+##active_users = cursor.fetchall()
+##print(active_users)
+##
+##con.close()
+
+
+#! Модуль
 import sqlite3
 
-with sqlite3.connect("db.db") as con:
-    cursor = con.cursor()
-    try:
-        with con:
-            cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user8", "user8@gmail.com"))
-            cursor.execute("INSERT INTO Users (username, email) VALUES (?, ?)", ("user9", "user9@gmail.com"))
-    except:
-        pass
+#! Подключение, курсор
+con = sqlite3.connect("db_2.db")
+cursor = con.cursor()
+
+###! Запрос
+##cursor.execute("""
+##CREATE TABLE IF NOT EXISTS Users (
+##id INTEGER PRIMARY KEY,
+##username TEXT NOT NULL,
+##email TEXT NOT NULL,
+##age INTEGER NOT NULL,
+##created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+##)
+##""")
+##
+###! Триггер
+##cursor.execute("""
+##CREATE TRIGGER IF NOT EXISTS update_created_at
+##AFTER INSERT ON Users
+##BEGIN
+##UPDATE Users SET created_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+##END
+##""")
+
+#! Индекс
+cursor.execute("CREATE INDEX idx_username ON Users (username)")
+
+con.commit()
+con.close()
